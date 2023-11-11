c3c5f964d4b229b0096e8b2e9aef8d4448442d28
DIYOUH PETER <102492516+Petasco@users.noreply.github.com>
2023-11-11 05:12:14 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'qoute.dart';
+
+void main() {
+  runApp( MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(text: "Be yourself, everyone is already taken", author: "Oscar Wild"),
+    Quote(text: "I have nothing to declare except my genius", author: "Oscar Wild"),
+    Quote(text: "The truth is rarely pure and never simple", author: "Oscar Wild"),
+    Quote(text: "No matter how long you live, heaven is till not assured", author: "Petasco"),
+  ];
+
+  Widget quoteTemplate(quote){
+      return Card(
+        margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.black87,
+              ),
+              ),
+              SizedBox(height: 6.0),
+
+            Text(quote.author,
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.black87,
+            ),
+      ),
+            ],
+          ),
+        ),
+      );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text("Awesome Quotes App",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+
+      body: Column(
+        children: quotes.map((quote) {
+          return quoteTemplate(quote);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+
