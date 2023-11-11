f7e160c1f622fef1e30f32a6889e34372035b41e
DIYOUH PETER <102492516+Petasco@users.noreply.github.com>
2023-11-11 09:50:50 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'qoute.dart';
+import 'quote_card.dart';
 
 void main() {
   runApp( MaterialApp(
@@ -24,31 +25,7 @@ class _QuoteListState extends State<QuoteList> {
   ];
 
   Widget quoteTemplate(quote){
-      return Card(
-        margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.black87,
-              ),
-              ),
-              SizedBox(height: 6.0),
-
-            Text(quote.author,
-            style: TextStyle(
-              fontSize: 18.0,
-              color: Colors.black87,
-            ),
-      ),
-            ],
-          ),
-        ),
-      );
+      return new QuoteCard(quote: quote);
   }
   @override
   Widget build(BuildContext context) {
@@ -74,5 +51,6 @@ class _QuoteListState extends State<QuoteList> {
     );
   }
 }
+
 
 
