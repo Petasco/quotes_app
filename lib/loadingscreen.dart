b4a0b5be57c410c0ab77415f3960f629a382c21c
DIYOUH PETER <102492516+Petasco@users.noreply.github.com>
2023-12-04 01:34:46 +0000

    Update loadingscreen.dart

--- lib/loadingscreen.dart
+++ lib/loadingscreen.dart
@@ -16,7 +16,7 @@ class _LoadingState extends State<Loading> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 5), () {
+    Future.delayed(const Duration(seconds: 5), () {
       loadHomePage(context);
     });
   }
